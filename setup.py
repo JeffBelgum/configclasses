aeb1e291f8748794fae12a2a46894b4d380684f1
Jeff Belgum <jeffbelgum@gmail.com>
2018-02-25 21:41:39 -0800

    update setup.py version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='configclasses',
-    version='0.1',
+    version='0.1.2',
     description='Strongly typed configuration classes made simple and effective.',
     url='http://github.com/jeffbelgum/configclasses',
     author='Jeff Belgum',
@@ -15,7 +15,7 @@ setup(
         'toml>=0.9.4',
     ],
     extras_require={
-        'dev': ['check-manifest'],
+        'dev': [],
         'test': ['pytest', 'pytest-cov'],
     },
     classifiers=[
