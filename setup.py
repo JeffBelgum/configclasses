fb365e4ed9e62a4120449868c17f39d509b5a466
Jeff Belgum <jeffbelgum@gmail.com>
2018-02-27 08:49:23 -0800

    patch version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='configclasses',
-    version='0.1.3',
+    version='0.1.4',
     description='Strongly typed configuration classes made simple and effective.',
     url='http://github.com/jeffbelgum/configclasses',
     author='Jeff Belgum',
